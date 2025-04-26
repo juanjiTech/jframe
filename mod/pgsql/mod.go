0ae06b84fa908e7a1a11ebe00e95ed9798323218
杨成锴 <homeboyc@foxmail.com>
2025-04-26 20:16:49 +0800

    feat: add postgress module

---
+++ mod/pgsql/mod.go
@@ -0,0 +1,65 @@
+package pgsql
+
+import (
+	"errors"
+	"fmt"
+	"github.com/juanjiTech/jframe/core/kernel"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var _ kernel.Module = (*Mod)(nil)
+
+type Config struct {
+	Host     string `yaml:"host"`
+	PORT     string `yaml:"port"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	Name     string `yaml:"name"`
+	SSLMode  string `yaml:"sslMode"`
+}
+
+type Mod struct {
+	kernel.UnimplementedModule // 请为所有Module引入UnimplementedModule
+
+	config Config
+}
+
+func (m *Mod) Name() string {
+	return "postgres"
+}
+
+func (m *Mod) Config() any { return &m.config }
+
+func (m *Mod) PreInit(hub *kernel.Hub) error {
+	c := m.config
+	if c.PORT == "" {
+		c.PORT = "5432"
+	}
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		c.User, c.Password, c.Host, c.PORT, c.Name, c.SSLMode)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+
+	hub.Log.Info("postgres init success")
+	hub.Map(&db)
+	return nil
+}
+
+func (m *Mod) Init(hub *kernel.Hub) error {
+	// check if inject success
+	var db *gorm.DB
+	if hub.Load(&db) != nil {
+		return errors.New("can't load gorm from kernel")
+	}
+
+	var tables []string
+	result := db.Raw("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'").Scan(&tables)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
